87857bcff6b14816fd52a1e4f6f5eb9e8f5a6287
Ryan Poh <ryan.poh@gmail.com>
2022-12-15 01:15:07 +0800

    fix for android termux port

--- config.py
+++ config.py
@@ -5,8 +5,9 @@ client_hostname = socket.gethostname()
 
 # Server side settings
 server_ip = '192.168.100.50'  # weasel ip (server)
-server_hostname = 'android-2.local'  # android termux
-server_username = 'XXXXX'  # weasel
+# android termux # android termux ssh access guide (https://joeprevite.com/ssh-termux-from-computer/)
+server_hostname = '192.168.100.50'
+server_username = 'u0_a764'  # weasel
 
 # server_savecopy_path = '/home/weasel/Desktop/LOOT/'  # for kali linux
 server_savecopy_path = '/data/data/com.termux/files/home/'  # for android Termux
@@ -17,7 +18,8 @@ server_savecopy_path = '/data/data/com.termux/files/home/'  # for android Termux
 server_port = 4000
 
 # for copy files. need to always turn on ssh in kali. "sudo service ssh start"
-server_ssh_port = 22
+server_ssh_port = 8022  # for Termux default
+# server_ssh_port = 22 # for pi or kali
 
 # Client side settings
 client_savecopy_path = (
